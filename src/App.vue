6163f13dc885182323c8834f871acbcb113ac9b1
123Fang <38906235+123Fang@users.noreply.github.com>
2024-03-19 12:29:59 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Repl } from '@vue/repl';
+import CodeMirror from '@vue/repl/codemirror-editor';
+import { watchEffect } from 'vue';
+
+const testData ={}
+</script>
+
+<template>
+    <Repl
+    :editor="CodeMirror"
+    :store="testData"
+    :show-import-map="false"
+    :show-ts-config="false"
+    @keydown.ctrl.s.prevent
+    @keydown.meta.s.prevent
+  />
+</template>
+
+<style>
+</style>
